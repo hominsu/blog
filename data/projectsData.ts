734fe4a7476c945091472e4ced49d121fd93709b
Homing So <homingso@foxmail.com>
2023-04-11 21:25:44 +0800

    feat: initial commit
    Signed-off-by: Homing So <homingso@foxmail.com>

---
+++ data/projectsData.ts
@@ -0,0 +1,27 @@
+const projectsData = [
+  {
+    title: 'neujson',
+    description: `neujson is a JSON parser and generator for C++. 
+    It supports both SAX and DOM style API.`,
+    href: 'https://github.com/hominsu/neujson',
+  },
+  {
+    title: 'bencode',
+    description: `A header-only Bencode parser and generator for C++17. 
+    It supports both SAX and DOM style API.`,
+    href: 'https://github.com/hominsu/bencode',
+  },
+  {
+    title: 'slink',
+    description: `A short link application base on kratos`,
+    href: 'https://github.com/hominsu/slink',
+  },
+  {
+    title: 'htp-platform',
+    description: `An Intelligent phenotype Determination System for 
+    High value-added Crops Based on a novel Rope-driven Parallel Robot`,
+    href: 'https://github.com/ynu-e-young/htp-platform',
+  },
+]
+
+export default projectsData
