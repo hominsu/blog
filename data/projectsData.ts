5586a7adb1396d42af7b49e374174b62403f15c1
Homing So <homingso@foxmail.com>
2023-04-11 21:36:29 +0800

    fix: imgSrc
    Signed-off-by: Homing So <homingso@foxmail.com>

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -3,23 +3,27 @@ const projectsData = [
     title: 'neujson',
     description: `neujson is a JSON parser and generator for C++. 
     It supports both SAX and DOM style API.`,
+    imgSrc: "",
     href: 'https://github.com/hominsu/neujson',
   },
   {
     title: 'bencode',
     description: `A header-only Bencode parser and generator for C++17. 
     It supports both SAX and DOM style API.`,
+    imgSrc: "",
     href: 'https://github.com/hominsu/bencode',
   },
   {
     title: 'slink',
     description: `A short link application base on kratos`,
+    imgSrc: "",
     href: 'https://github.com/hominsu/slink',
   },
   {
     title: 'htp-platform',
     description: `An Intelligent phenotype Determination System for 
     High value-added Crops Based on a novel Rope-driven Parallel Robot`,
+    imgSrc: "",
     href: 'https://github.com/ynu-e-young/htp-platform',
   },
 ]
