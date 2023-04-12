43eb28d636fe2ffff250f2f33f02275d621410db
Homing So <homingso@foxmail.com>
2023-04-12 11:58:00 +0800

    feat: modified email provider
    Signed-off-by: Homing So <homingso@foxmail.com>

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -26,7 +26,7 @@ const siteMetadata = {
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection
-    provider: 'emailoctopus',
+    provider: 'buttondown',
   },
   comments: {
     // If you want to use an analytics provider you have to add it to the
