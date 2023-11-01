6c7dbacf67b2d7554e2621a0991d0f21b398c646
Homing So <homingso@foxmail.com>
2023-11-01 23:38:48 +0800

    feat: bump to v2
    Signed-off-by: Homing So <homingso@foxmail.com>

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,30 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'neujson',
+    description: `neujson is a JSON parser and generator for C++. 
+    It supports both SAX and DOM style API.`,
+    imgSrc: "",
+    href: 'https://github.com/hominsu/neujson',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'bencode',
+    description: `A header-only Bencode parser and generator for C++17. 
+    It supports both SAX and DOM style API.`,
+    imgSrc: "",
+    href: 'https://github.com/hominsu/bencode',
+  },
+  {
+    title: 'slink',
+    description: `A short link application base on kratos`,
+    imgSrc: "",
+    href: 'https://github.com/hominsu/slink',
+  },
+  {
+    title: 'htp-platform',
+    description: `An Intelligent phenotype Determination System for 
+    High value-added Crops Based on a novel Rope-driven Parallel Robot`,
+    imgSrc: "",
+    href: 'https://github.com/ynu-e-young/htp-platform',
   },
 ]
 
