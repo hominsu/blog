14571f574f829ee47ff327605b6d59e224869fb0
Homing So <homingso@foxmail.com>
2023-11-02 22:37:12 +0800

    feat: modified search provider to algolia
    Signed-off-by: Homing So <homingso@foxmail.com>

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -74,18 +74,18 @@ const siteMetadata = {
     },
   },
   search: {
-    provider: 'kbar', // kbar or algolia
-    kbarConfig: {
-      searchDocumentsPath: `${process.env.BASE_PATH || ''}/search.json`, // path to load documents to search
-    },
-    // provider: 'algolia',
-    // algoliaConfig: {
-    //   // The application ID provided by Algolia
-    //   appId: 'R2IYF7ETH7',
-    //   // Public API key: it is safe to commit it
-    //   apiKey: '599cec31baffa4868cae4e79f180729b',
-    //   indexName: 'docsearch',
+    // provider: 'kbar', // kbar or algolia
+    // kbarConfig: {
+    //   searchDocumentsPath: `${process.env.BASE_PATH || ''}/search.json`, // path to load documents to search
     // },
+    provider: 'algolia',
+    algoliaConfig: {
+      // The application ID provided by Algolia
+      appId: process.env.NEXT_ALGOLIA_APPID,
+      // Public API key: it is safe to commit it
+      apiKey: process.env.NEXT_ALGOLIA_APIKEY,
+      indexName: process.env.NEXT_ALGOLIA_INDEX_NAME,
+    },
   },
 }
 
