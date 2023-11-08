7f7a7f03e65e50fa6e0d4803a318cac25007f9c5
Homing So <homingso@foxmail.com>
2023-11-08 20:23:18 +0800

    feat: add banner for projects
    Signed-off-by: Homing So <homingso@foxmail.com>

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -3,27 +3,27 @@ const projectsData = [
     title: 'neujson',
     description: `neujson is a JSON parser and generator for C++. 
     It supports both SAX and DOM style API.`,
-    imgSrc: "",
+    imgSrc: '/static/images/projects/neujson/banner.png',
     href: 'https://github.com/hominsu/neujson',
   },
   {
     title: 'bencode',
     description: `A header-only Bencode parser and generator for C++17. 
     It supports both SAX and DOM style API.`,
-    imgSrc: "",
+    imgSrc: '/static/images/projects/bencode/banner.png',
     href: 'https://github.com/hominsu/bencode',
   },
   {
     title: 'slink',
     description: `A short link application base on kratos`,
-    imgSrc: "",
+    imgSrc: '',
     href: 'https://github.com/hominsu/slink',
   },
   {
     title: 'htp-platform',
     description: `An Intelligent phenotype Determination System for 
     High value-added Crops Based on a novel Rope-driven Parallel Robot`,
-    imgSrc: "",
+    imgSrc: '',
     href: 'https://github.com/ynu-e-young/htp-platform',
   },
 ]
