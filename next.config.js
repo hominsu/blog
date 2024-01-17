d4bd65353bcfd4dfced4d69003ae263bfb82c77e
Homing So <homingso@foxmail.com>
2024-01-17 20:15:27 +0800

    feat: add rewrites
    Signed-off-by: Homing So <homingso@foxmail.com>

--- next.config.js
+++ next.config.js
@@ -54,6 +54,15 @@ const securityHeaders = [
   },
 ]
 
+const articleMapping = new Map([
+  ['e13e6093', 'bit-hacks/compute-abs'],
+  ['1ddd54b8', 'bit-hacks/compute-power2'],
+  ['438146ef', 'bit-hacks/count-bits'],
+  ['4edea31e', 'other/cpp-calls-ffmpeg-for-rtmp-push'],
+  ['a0caaef2', 'other/design-and-implementation-of-single-cycle-cpu'],
+  ['d6e69539', 'other/trust-project-env.mdx'],
+])
+
 /**
  * @type {import('next/dist/next-server/server/config').NextConfig}
  **/
@@ -76,6 +85,12 @@ module.exports = () => {
           headers: securityHeaders,
         },
       ]
+    },
+    async rewrites() {
+      return [...articleMapping].map(([slug, destination]) => ({
+        source: `/article/${slug}.html`,
+        destination: `/blog/${destination}`,
+      }))
     },
     webpack: (config, options) => {
       config.module.rules.push({
